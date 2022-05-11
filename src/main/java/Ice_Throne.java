ee0e9dbb1812086df49352926b1d3e31c9289830
Sakura <73380597+1300395346@users.noreply.github.com>
2022-05-11 16:33:14 +0800

    寒冰王座新增一个贪心解法，时间复杂度为O(1)

--- src/main/java/Ice_Throne.java
+++ src/main/java/Ice_Throne.java
@@ -4,20 +4,33 @@ public class Ice_Throne {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int len = scanner.nextInt();
-        int[] value = {0, 150, 200, 350};
-        int[] dp = new int[10000];
-        while (len-- > 0) {
+        //贪心算法
+        while(len-- > 0){
             int temp = scanner.nextInt();
-            for (int i = 0; i < 3; ++i) {
-                for (int j = 1; j <= temp; ++j) {
-                    if (j >= value[i]) {
-                        dp[j] = Math.max(dp[j], dp[j - value[i]] + value[i]);
-                    }
-                }
+            int xy = temp/150;
+            int x = temp % 150 / 50;
+            if (x > xy){
+                x = xy;
             }
-            System.out.println(temp - dp[temp]);
+            System.out.println(xy);
+            System.out.println(x);
+            System.out.println(temp-150*xy-50*x);
         }
-
+        //DP
+//        int[] value = {0, 150, 200, 350};
+//        int[] dp = new int[10000];
+//        while (len-- > 0) {
+//            int temp = scanner.nextInt();
+//            for (int i = 0; i < 3; ++i) {
+//                for (int j = 1; j <= temp; ++j) {
+//                    if (j >= value[i]) {
+//                        dp[j] = Math.max(dp[j], dp[j - value[i]] + value[i]);
+//                    }
+//                }
+//            }
+//            System.out.println(temp - dp[temp]);
+//        }
+//
         scanner.close();
     }
 }
